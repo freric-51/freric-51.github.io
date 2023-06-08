c1402a609e4b475db3734170438ed06b11acde37
freric-51 <ricdefreitas@hotmail.com>
2023-06-08 09:56:28 -0300

    "2023 revision 01"

--- src/App.js
+++ src/App.js
@@ -8,16 +8,18 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
             <h2>Ricardo</h2>
-          Edit <code>src/App.js</code> and save to reload.
+            {/* Edit <code>src/App.js</code> and save to reload.  */}
         </p>
-        <a
+
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+
       </header>
     </div>
   );
