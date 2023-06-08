56f9e3d4c4d1d815a1906e584d9f777269095881
freric-51 <ricdefreitas@hotmail.com>
2023-06-08 16:45:59 -0300

    "2023 revision 01"

--- src/index.js
+++ src/index.js
@@ -14,16 +14,29 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-function sendToAnalytics(metric) {
-    const body = JSON.stringify(metric);
-    const url = 'https://freric-51.github.io/analytics';
-
-    // Use `navigator.sendBeacon()` if available, falling back to `fetch()`
-    if (navigator.sendBeacon) {
-      navigator.sendBeacon(url, body);
-    } else {
-      fetch(url, { body, method: 'POST', keepalive: true });
-    }
+function sendToAnalytics({ id, name, value }) {
+    reportWebVitals.ga('send', 'event', {
+      eventCategory: 'Web Vitals',
+      eventAction: name,
+      eventValue: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+      eventLabel: id, // id unique to current page load
+      nonInteraction: true, // avoids affecting bounce rate
+    });
   }
-reportWebVitals(sendToAnalytics);
+
+// function sendToAnalytics(metric) {
+//     const body = JSON.stringify(metric);
+//     const url = 'https://freric-51.github.io/analytics';
+
+//     // Use `navigator.sendBeacon()` if available, falling back to `fetch()`
+//     if (navigator.sendBeacon) {
+//       navigator.sendBeacon(url, body);
+//     } else {
+//       fetch(url, { body, method: 'POST', keepalive: true });
+//     }
+//   }
+
+
+
+reportWebVitals(sendToAnalytics('G-4W0ESB57EC'));
 
