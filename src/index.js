a589ac578afee32ed7a6ff3767c63989e2dd84ad
freric-51 <ricdefreitas@hotmail.com>
2023-06-08 13:12:46 -0300

    "2023 revision 01"

--- src/index.js
+++ src/index.js
@@ -14,4 +14,16 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function sendToAnalytics(metric) {
+    const body = JSON.stringify(metric);
+    const url = 'https://freric-51.github.io/analytics';
+
+    // Use `navigator.sendBeacon()` if available, falling back to `fetch()`
+    if (navigator.sendBeacon) {
+      navigator.sendBeacon(url, body);
+    } else {
+      fetch(url, { body, method: 'POST', keepalive: true });
+    }
+  }
+reportWebVitals(sendToAnalytics);
+
