c1488ec70dc81fac0a9dea694505ed9b257cfe7a
freric-51 <ricdefreitas@hotmail.com>
2023-06-24 08:01:33 -0300

    "2023 revision 01"

--- src/index.js
+++ src/index.js
@@ -8,21 +8,24 @@ import reportWebVitals from './reportWebVitals';
 // eslint-disable-next-line
 import { BrowserRouter } from 'react-router-dom';
 import ReactGA from "react-ga4";
-import { FormattedMessage } from 'react-intl'
+
+// import {IntlProvider, FormattedMessage, FormattedNumber} from 'react-intl'
+// addLocaleData é uma função que provê uma maneira de registrar os dados locais da biblioteca
+import { addLocaleData } from 'react-intl'
+// IntlProvider é usado para configurar o contexto i18n,
+//              deve ser registrado na raiz para que toda a aplicação possa ser internacionalizada
+import { IntlProvider }  from 'react-intl'
+
+// flattenMessages é a função que nivela os textos
+// e messages é o objeto com as mensagens de acordo com o idioma
+import { flattenMessages } from './_i18n/utils'
+import messages from './_i18n/messages'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement );
 
-
 //Initialize GA4
 ReactGA.initialize("G-4W0ESB57EC");
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
 const SendAnalytics = ()=> {
     ReactGA.send({
       hitType: "pageview",
@@ -30,6 +33,14 @@ const SendAnalytics = ()=> {
     });
   }
 
-  reportWebVitals(SendAnalytics);
+reportWebVitals(SendAnalytics);
 
+
+root.render(
+  <React.StrictMode>
+    <IntlProvider locale='pt' defaultLocale="pt">
+        <App />
+    </IntlProvider>
+  </React.StrictMode>
+);
 
